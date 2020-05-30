d843d31232c905387b1464b16187bc4295378401
Alek Brown <alekjbrown@live.com>
2020-05-30 18:27:39 -0500

    DataTypeSizeBrown assignment complete

---
+++ DataTypeSize/DataTypeSizeBrown.cpp
@@ -0,0 +1,18 @@
+//Aleksander Brown CIS 161 DataTypeSizeBrown.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    char letter;
+    int num1;
+    float num2;
+    double num3;
+
+    cout << "char size = " << sizeof(letter) << endl;
+    cout << "int size = " << sizeof(num1) << endl;
+    cout << "float size = " << sizeof(num2) << endl;
+    cout << "double size = " << sizeof(num3) << endl;
+}
